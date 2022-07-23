656e184b452c935e499eae1357d305fd888a7cc3
Zeynep Güneş Özkan <zeynepgunesozkan@zeyneps-air.home>
2022-07-23 02:30:30 +0100

    new plot

--- Data_process.R
+++ Data_process.R
@@ -15,69 +15,51 @@ save(raw_data2, file= "data/raw_data2.Rda")
 
 write.csv(raw_data2, "data/raw_data2.csv")
 
+
 library("viridis") 
 
 library(ggplot2)
-ggplot(raw_data, aes(x= cond, y=fix_dur, col= fix_dur))+
+library(tidyquant)
+library(tidyverse)
+library(ggforce)
+library(ggdist)
+pallete1= c("#CA3542", "#27647B", "#849FA0", "#AECBC9", "#57575F")
+ggplot(raw_data, aes(x= cond, y=fix_dur))+
   geom_jitter()+
   theme_minimal()
 
+ggplot(dat, aes(x= cond, fix_dur, fill=cond)) +
+  ggdist::stat_halfeye(adjust = .5, width = .3, .width = 0, justification = -.4) + 
+  geom_boxplot(width = .1, outlier.shape = NA) +
+  ggdist::stat_dots(side = "left", dotsize = .3, justification = 1.1, binwidth = 15,col="dark green")+
+  theme_classic()+
+  scale_fill_manual(values=pallete1[3:4])+
+  coord_flip()  
 
-### Bar Charts (just to try) ###
 
-library(reshape)
 
-subdat<- melt(raw_data, id=c('sub', 'item','cond'), 
-             measure=c("fix_dur", 'sacc_dur', 'sacc_ampl', 'peak_vel'),na.rm = TRUE)
-MSD<- cast(subdat, cond ~ variable
-           ,function(x) c(M=signif(mean(x),3)
-                          , SD= sd(x) ))
+#Analysis attemp :D
 
-MSD$fix_dur_SE <- sqrt(MSD$fix_dur_SD)
+library(lme4)
+lmer(fix_dur ~ cond + (1|item), data = raw_data)
 
-fix_bar <- ggplot(MSD) +
-  geom_bar( aes(x=cond, y=fix_dur_M), stat="identity",fill="steelblue",width = 0.4) +
-  geom_errorbar( aes(x=cond, ymin=fix_dur_M-fix_dur_SE, ymax=fix_dur_M+fix_dur_SE),
-                 width=0.1, colour="black", alpha=0.9, size=0.7) +
-  ylab("Mean Fixation Duration")+
-  xlab("Condition")+
-  ylim(0, 350)+
-  ggtitle("Bars represent standard errors")
-fix_bar
+dat <- raw_data
 
-MSD$sacc_dur_SE <- sqrt(MSD$sacc_dur_SD)
+#Centering fix_dur
+dat$fix_dur_c <- scale(dat$fix_dur, scale = F)
 
-sacc_bar <- ggplot(MSD) +
-  geom_bar( aes(x=cond, y=sacc_dur_M), stat="identity",fill="steelblue",width = 0.4) +
-  geom_errorbar( aes(x=cond, ymin=sacc_dur_M-sacc_dur_SE, ymax=sacc_dur_M+sacc_dur_SE),
-                 width=0.1, colour="black", alpha=0.9, size=0.7) +
-  ylab("Mean Saccade Duration")+
-  xlab("Condition")+
-  ggtitle("Bars represent standard errors")
-sacc_bar
+#Contrast
+dat$cond= as.factor(dat$cond)
+levels(dat$cond)
+contrasts(dat$cond)
+#Regression
+LinReg = lm(formula = fix_dur ~ cond + (1|item) + (1+cond|sub), data = dat)
+#lm(least squares) gives singularity error but lmer(random effects) not why?
+summary(LinearRegression)
 
-MSD$peak_vel_SE <- sqrt(MSD$peak_vel_SD)
 
-velo_bar <- ggplot(MSD) +
-  geom_bar( aes(x=cond, y=peak_vel_M), stat="identity",fill="steelblue",width = 0.4) +
-  geom_errorbar( aes(x=cond, ymin=peak_vel_M-peak_vel_SE, ymax=peak_vel_M+peak_vel_SE),
-                 width=0.1, colour="black", alpha=0.9, size=0.7) +
-  ylab("Mean Saccade Velocity")+
-  xlab("Condition")+
-  ggtitle("Bars represent standard errors")
-velo_bar
-
-MSD$sacc_ampl_SE <- sqrt(MSD$sacc_ampl_SD)
-
-ampl_bar <- ggplot(MSD) +
-  geom_bar( aes(x=cond, y=sacc_ampl_M), stat="identity",fill="steelblue",width = 0.4) +
-  geom_errorbar( aes(x=cond, ymin=sacc_ampl_M-sacc_ampl_SE, ymax=sacc_ampl_M+sacc_ampl_SE),
-                 width=0.1, colour="black", alpha=0.9, size=0.7) +
-  ylab("Mean Saccade Amplitude")+
-  xlab("Condition")+
-  ggtitle("Bars represent standard errors")
-ampl_bar
-
+#fixed effects
+#cond
 
 
 
