20a862d5d2cedaa8810530ccf3c77222cccfe31e
Bernhard Angele <bangele@bournemouth.ac.uk>
2022-12-18 16:39:08 +0000

    fix save commands for the model, add iterations to some that didn't have 1000/5000

--- Bayesian_Analysis.R
+++ Bayesian_Analysis.R
@@ -91,7 +91,7 @@ blmm_ben_skip <- function(dataA, word_n, warmup = 1000, iter = 5000, chains = 4,
 # First Fixation Duration / Target word (N)
 
 blm_ffd_n <- blmm_ben(dataA = fixation_time_measures_withdelete, word_n = 0, dv = "ffd", warmup = 1000, iter = 5000)
-save(blmm_ffd_n, "blmm_ffd_n.RData")
+save(blm_ffd_n, file = "blmm_ffd_n.RData")
 summary(blm_ffd_n)
 
 # Skipping / Word N
@@ -100,28 +100,28 @@ summary(blm_ffd_n)
 
 #lm_skip <- glmer(data = fixation_time_measures %>% filter(wordN == boundaryN ), skipping ~ target_changed * scale(inhibition_score) + (1|sub), family = binomial(link = "logit")) 
 blm_skip_n <- blmm_ben_skip(data = fixation_time_measures_withdelete, word_n = 0, warmup = 1000, iter = 5000)
-save(blm_skip_n, "blm_skip_n.RData")
+save(blm_skip_n, file = "blm_skip_n.RData")
 summary(blm_skip_n)
 
 
 # Gaze Duration / Word N
 
 blm_gd_n <- blmm_ben(data = fixation_time_measures_withdelete ,word_n = 0, dv = "gd", warmup = 1000, iter = 5000)
-save(blm_gd_n, "blm_gd_n.RData")
+save(blm_gd_n, file = "blm_gd_n.RData")
 summary(blm_gd_n)
 
 
 # Go-past Time / Word N
 
 blm_gp_n <- blmm_ben(data = fixation_time_measures_withdelete, word_n = 0, dv = "gopast", warmup = 1000, iter = 5000)
-save(blm_gp_n, "blm_gp_n.RData")
+save(blm_gp_n, file = "blm_gp_n.RData")
 summary(blm_gp_n)
 
 
 # Total View Time / Word N
 
 blm_tvt_n <- blmm_ben(data = fixation_time_measures_withdelete ,word_n = 0, dv = "tvt", warmup = 1000, iter = 5000)
-save(blm_tvt_n, "blm_tvt_n.RData")
+save(blm_tvt_n, file = "blm_tvt_n.RData")
 summary(blm_tvt_n)
 
 ### N+1 ###
@@ -129,40 +129,40 @@ summary(blm_tvt_n)
 # First fixation duration / Word N + 1
 
 blm_ffd_n1 <- blmm_ben(data = fixation_time_measures_withdelete, word_n = 1, dv = "ffd", warmup = 1000, iter = 5000)
-save(blmm_ffd_n1, "blmm_ffd_n1.RData")
+save(blmm_ffd_n1, file = "blmm_ffd_n1.RData")
 summary(blm_ffd_n1)
 
 # Skipping / Word N + 1
 
 #lm_skip1 <- glmer(data = fixation_time_measures %>% filter(wordN == boundaryN + 1), skipping ~ target_changed * scale(inhibition_score) + (1|sub), family = binomial(link = "logit")) 
 blm_skip_n1 <- blmm_ben_skip(data = fixation_time_measures_withdelete, word_n = 1, warmup = 1000, iter = 5000)
-save(blm_skip_n1, "blm_skip_n1.RData")
+save(blm_skip_n1, file = "blm_skip_n1.RData")
 summary(blm_skip_n1)
 
 # Gaze duration / Word N + 1
 
 blm_gd_n1 <- blmm_ben(data = fixation_time_measures_withdelete, word_n = 1, dv = "gd", warmup = 1000, iter = 5000)
-save(blm_gd_n1, "blm_gd_n1.RData")
+save(blm_gd_n1, file = "blm_gd_n1.RData")
 summary(blm_gd_n1)
 
 
 # Go-past Time / Word N + 1
 
-blm_gp_n1 <- blmm_ben(data = fixation_time_measures_withdelete, word = (boundaryN + 1), dv = "gp", warmup = 10, iter = 50)
-save(blm_gp_n1, "blm_gp_n1.RData")
+blm_gp_n1 <- blmm_ben(data = fixation_time_measures_withdelete, word = (boundaryN + 1), dv = "gp", warmup = 1000, iter = 5000)
+save(blm_gp_n1, file = "blm_gp_n1.RData")
 summary(blm_gp_n1)
 
 # TVT / Word N + 1
 
-blm_tvt_n1 <- blmm_ben(data = fixation_time_measures_withdelete, word = (boundaryN + 1), dv = "tvt", warmup = 10, iter = 50)
-save(blm_tvt_n1, "blm_tvt_n1.RData")
+blm_tvt_n1 <- blmm_ben(data = fixation_time_measures_withdelete, word = (boundaryN + 1), dv = "tvt", warmup = 1000, iter = 5000)
+save(blm_tvt_n1, file = "blm_tvt_n1.RData")
 summary(blm_tvt_n1)
 
 
 # Gaze Duration / Word N - 1
 
 blm_gd_n1m <- blmm_ben(data = fixation_time_measures_withdelete, word_n = - 1, dv = "gd", warmup = 1000, iter = 5000)
-save(blm_gd_n1m, "blm_gd_n1m.RData")
+save(blm_gd_n1m, file = "blm_gd_n1m.RData")
 summary(blm_tvt_n1m)
 
 # Skipping N -1
@@ -170,6 +170,6 @@ summary(blm_tvt_n1m)
 #lm_skip1m <- glmer(data = fixation_time_measures %>% filter(wordN == boundaryN - 1), skipping ~ target_changed * scale(inhibition_score) + (1|sub), family = binomial(link = "logit")) 
 
 blm_skip_n1m <- blmm_ben_skip(data = fixation_time_measures_withdelete, word_n = -1, warmup = 1000, iter = 5000)
-save(blm_skip_n1m, "blm_skip_n1m.RData")
+save(blm_skip_n1m, file = "blm_skip_n1m.RData")
 summary(blm_skip_n1m)
 
